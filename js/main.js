902e4671ea4c18f951f2cd8b367dbd72bbb1b61a
FrankLuo426 <caesarfrank@hotmail.com>
2020-04-23 11:28:33 -0400

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,69 @@
+"use strict";
+
+
+
+// Constants
+
+
+// Pre-load the images
+
+
+// Game State enum
+const gameState = Object.freeze({
+    StartScene: 0,
+    GameScene: 1,
+    ControlScene: 2,
+    GameOverScene: 3
+});
+
+const enemyType = Object.freeze({
+    Water: 1,
+    Lava: 2,
+    Goo: 3,
+    Poison: 4,
+    Chocolate: 5,
+    Pee: 6,
+    Ice: 7,
+    Milk: 8
+});
+
+// Aliases
+let stage;
+
+// Field for local storage
+
+// Field for game variables
+
+// Game Scene variables
+
+/// Set up the scenes
+function setup() {
+   
+}
+
+// For creatings labels and buttons
+function createLabelsAndButtons() {
+  
+}
+
+function quitGame() {
+    
+}
+
+function viewControls() {
+    
+}
+
+function fromControlsToStart() {
+   
+}
+
+// Clicking the button calls startGame()
+function startGame() {
+    
+}
+
+// Load game over scene
+function end() {
+  
+}
